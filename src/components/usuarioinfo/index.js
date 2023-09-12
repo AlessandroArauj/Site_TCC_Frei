451a49c77b98ba580ef9e356b050d730529f8098
DenisCromy <denistodias1@gmail.com>
2023-09-12 18:25:14 -0300

    falta os inputs

--- src/components/usuarioinfo/index.js
+++ src/components/usuarioinfo/index.js
@@ -6,65 +6,77 @@ export default function AbaUsuario() {
 
         <div className='aba-usuario'>
 
-            <header className='mini-head'>
+            <div className='superior'>
 
-                <img className='img-usu-aba' src='../../../assets/images/logo.svg'></img>
+                <div className='options'>
 
-                <div className='infos'>
-                    <h2>Name</h2>
-                    <div className='linhafina'></div>
-                    <p>email</p>
+                    <div className='cima'>
+                        <img src='../../../assets/images/logo.svg' />
+                        <div>
+                            <h1>Nome</h1>
+                            <div className='line' />
+                            <p>Email</p>
+                        </div>
+                    </div>
+
+
+                    <div className='baixo'>
+
+
+                        <div className='buttons'>
+                            <article className='botao-aba'>
+
+                                <button>
+                                    <p>HOME</p>
+                                    <img src='../../assets/images/image142.svg'></img>
+                                </button>
+
+
+                            </article>
+
+                            <article className='botao-aba'>
+
+                                <button>
+                                    <p>EDITAR PERFIL</p>
+                                    <img src='../../assets/images/image138.svg'></img>
+                                </button>
+
+                            </article>
+
+                            <article className='botao-aba'>
+
+                                <button>
+                                    <p>NOTIFICAÇÕES</p>
+                                    <img src='../../assets/images/image137.svg'></img>
+                                </button>
+
+                            </article>
+
+                            <article className='botao-aba'>
+
+                                <button>
+                                    <p>SENHAS/SEGURANÇA</p>
+                                    <img src='../../assets/images/image139.svg'></img>
+                                </button>
+
+                            </article>
+
+
+                            <nav className='deslog-aba'>
+
+                                <button>Deslogar</button>
+
+                            </nav>
+
+                        </div>
+
+
+
+
+
+                    </div>
+
                 </div>
-            </header>
-            <div className='conteudo-aba'>
-
-                <div className='buttons'>
-                    <article className='botao-aba'>
-
-                        <button>
-                            <p>HOME</p>
-                            <img src='../../assets/images/image142.svg'></img>
-                        </button>
-
-
-                    </article>
-
-                    <article className='botao-aba'>
-
-                        <button>
-                            <p>EDITAR PERFIL</p>
-                            <img src='../../assets/images/image138.svg'></img>
-                        </button>
-
-                    </article>
-
-                    <article className='botao-aba'>
-
-                        <button>
-                            <p>NOTIFICAÇÕES</p>
-                            <img src='../../assets/images/image137.svg'></img>
-                        </button>
-
-                    </article>
-
-                    <article className='botao-aba'>
-
-                        <button>
-                            <p>SENHAS/SEGURANÇA</p>
-                            <img src='../../assets/images/image139.svg'></img>
-                        </button>
-
-                    </article>
-
-                </div>
-
-
-                <nav className='deslog-aba'>
-
-                    <button>Deslogar</button>
-
-                </nav>
-
             </div>
 
 
--- src/pages/USUÁRIO/perfilusuario/index.js
+++ src/pages/USUÁRIO/perfilusuario/index.js
@@ -1,6 +1,6 @@
 import './index.scss'
 import AbaUsuario from '../../../components/usuarioinfo'
-import Cabecalho from '../../../components/cabecalho'
+
 
 
 export default function UsarioPerfil(){
@@ -11,7 +11,7 @@ return(
         <AbaUsuario className='abazinha'></AbaUsuario>
 
 
-        <div className='faixa-cinza'></div>
+        
         
         
 
