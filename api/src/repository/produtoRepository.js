2a0b6779ad241a8721c4082a26b45f086ea62b65
Denis <denistodias1@gmail.com>
2023-09-17 21:52:31 -0300

    API finalizada

--- api/src/repository/produtoRepository.js
+++ api/src/repository/produtoRepository.js
@@ -1,5 +1,51 @@
 import { con } from "./connection.js";
 
+export async function DeletarProduto(id){
+    const comando = `
+        DELETE FROM TB_PRODUTO
+            WHERE   ID_INSTRUMENTOS = ?
+    
+    `
+
+    const [resp] = await con.query(comando, [id]);
+    return resp.affectedRows;
+
+}
+
+export async function AlterarProduto(produto, id){
+    const comando = `
+        UPDATE TB_PRODUTO
+        SET
+            ID_MARCAS               = ?,
+            ID_CATEGORIA            = ?,
+            NM_PRODUTO              = ?,
+            VL_PRECO                = ?,
+            VL_PRECO_PROMOCIONAL    = ?,
+            BT_DESTAQUE             = ?,
+            BT_PROMOCAO             = ?,
+            BT_DISPONIVEL           = ?,
+            QTD_ESTOQUE             = ?,
+            DS_DETALHES             = ?
+        WHERE ID_INSTRUMENTOS = ?
+    `
+
+    const [ resp ] = await con.query(comando, [
+        produto.MARCAS,
+        produto.CATEGORIA,
+        produto.PRODUTO,
+        produto.PRECO,
+        produto.PRECO_PROMOCIONAL,
+        produto.DESTAQUE,
+        produto.PROMOCAO,
+        produto.DISPONIVEL,
+        produto.ESTOQUE,
+        produto.DETALHE,
+        id
+    ])
+
+    return resp.affectedRows;
+}
+
 export async function inserirProduto (produto){
     const comando = `
     INSERT INTO TB_PRODUTO (ID_MARCAS, ID_CATEGORIA, NM_PRODUTO, VL_PRECO, VL_PRECO_PROMOCIONAL, BT_DESTAQUE, BT_PROMOCAO, BT_DISPONIVEL, QTD_ESTOQUE, DS_DETALHES) 
