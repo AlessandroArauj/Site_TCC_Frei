63ef3954fe45a7dafd7bb1f441257bc8d4fb4146
Alezin <araujobr1903@gmail.com>
2023-10-11 15:28:28 -0300

    atualizei a pagina de addcartao

--- site/src/pages/USUARIO/addcartao/index.js
+++ site/src/pages/USUARIO/addcartao/index.js
@@ -5,7 +5,46 @@ export default function AddCartao(){
 
     return(
         <div className='page-add-cartao'>
-            <Cabdois />
+
+
+                <div className='card'>
+
+                   <div className='titulo'>
+                    <h1 className='sair'>X</h1>
+                    <h1 className='forma'>ESCOLHER FORMA DE PAGAMENTO</h1>
+                    <div className='linha'></div>
+                    </div>
+
+                <div className='subcard'>
+
+                    <p className='subtitulo'>CARTOES CADASTRADOS</p>
+                    <div className='parcela'>
+                    <input className='cartao' type='text' ></input>
+                        
+                        <label className='labelzim'>
+                            <p className='testinho'>PARCELAR EM</p>
+                            <input className='numero_parcela' type='number'></input>
+                        </label>
+                        <button className='cadastrar'>CADASTRAR NOVO CARTAO</button>
+                    
+                    
+                    
+                    
+                    
+                    </div>
+                <div className='dd'>
+                <div className='divisinha'></div>
+                <p className='outras'>outras formas</p>
+                <div className='divisinha'></div>
+                </div>
+
+
+
+
+
+                </div>
+
+                </div>
 
 
 
