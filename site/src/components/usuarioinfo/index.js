5bd4f266778930ae6a21eb00da4ebb6b9dbfbf75
alezin <araujobr1903@gmail.com>
2023-10-16 00:14:49 -0300

    .

--- site/src/components/usuarioinfo/index.js
+++ site/src/components/usuarioinfo/index.js
@@ -1,114 +1,36 @@
 import './index.scss'
-import storage from 'local-storage'
-import { useNavigate } from 'react-router-dom'
-import { useEffect, useState } from 'react'
 
-export default function AbaUsuario() {
-    const navigate = useNavigate()
 
-    const [usuario, setUsuario] = useState('-');
-    const [email, setEmail] = useState('-');
+export default function conteudoOptions(){
+
+
+    return(
+        <div className='conteudoOption'>
 
 
 
-    useEffect(() => {
-        if (!storage('usuario-logado')){
-            navigate('/loginUser')
-        }
-        else {
-            const usuariologado = storage('usuario-logado');
-            setUsuario(usuariologado.nome)
-            
-        
-        }
-    }, [])
+            <section className='editarPerfil'>
+                <h1> Dados Cadastrais</h1>
+                <div className='line' />
 
-    function sairClick(){
-        
-    storage.remove('usuario-logado');
-    navigate('/')
-    }
-
-    return (
-
-        <div className='aba-usuario'>
-
-            <div className='superior'>
-
-                <div className='options'>
-
-                    <div className='cima'>
-                        <img src='../../../assets/images/logo.svg' />
-                        <div>
-                            <h1> {usuario}</h1>
-                            <div className='line' />
-                            <p>{email}</p>
-                        </div>
+                <div className='inputs'>
+                    <div>
+                        <input type='text' placeholder=' NOME'/>
+                        <input type='text' placeholder=' DATA NASCM'/>
+                        <input type='text' placeholder=' ENDEREÇO'/>
                     </div>
-
-
-                    <div className='baixo'>
-
-
-                        <div className='buttons'>
-                            <article className='botao-aba'>
-
-                                <button>
-                                    <p>HOME</p>
-                                    <img src='../../assets/images/image142.svg'></img>
-                                </button>
-
-
-                            </article>
-
-                            <article className='botao-aba'>
-
-                                <button>
-                                    <p>EDITAR PERFIL</p>
-                                    <img src='../../assets/images/image138.svg'></img>
-                                </button>
-
-                            </article>
-
-                            <article className='botao-aba'>
-
-                                <button>
-                                    <p>NOTIFICAÇÕES</p>
-                                    <img src='../../assets/images/image137.svg'></img>
-                                </button>
-
-                            </article>
-
-                            <article className='botao-aba'>
-
-                                <button>
-                                    <p>SENHAS/SEGURANÇA</p>
-                                    <img src='../../assets/images/image139.svg'></img>
-                                </button>
-
-                            </article>
-
-
-                            <nav className='deslog-aba'>
-
-                                <button onClick={sairClick}>Deslogar</button>
-
-                            </nav>
-
-                        </div>
-
-
-
-
-
+                    
+                    <div>
+                        <input type='text' placeholder=' TELEFONE'/>
+                        <input type='text' placeholder=' CPF'/>
+                        <input type='text' placeholder=' CIDADE'/>
                     </div>
-
                 </div>
-            </div>
 
+
+                <button > SALVAR ALTERAÇÕES</button>
+            </section>
 
         </div>
-
     )
-
 }
--- site/src/pages/USUARIO/perfilusuario/index.js
+++ site/src/pages/USUARIO/perfilusuario/index.js
@@ -1,28 +1,153 @@
 import './index.scss'
-import AbaUsuario from '../../../components/usuarioinfo'
+import storage from 'local-storage'
+import { useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import Modais from '../../../components/usuarioinfo/index.js'
+
+export default function AbaUsuario() {
+    const navigate = useNavigate()
+
+    const [usuario, setUsuario] = useState('-');
+    const [email, setEmail] = useState('-');
 
 
 
-export default function UsarioPerfil(){
-    
+    useEffect(() => {
+        if (!storage('usuario-logado')) {
+            navigate('/loginUser')
+        }
+        else {
+            const usuariologado = storage('usuario-logado');
+            setUsuario(usuariologado.nome)
 
 
-return(
-    
-    <div className='pagina-perfil-usuario'>
-        
+        }
+    }, [])
 
-        <AbaUsuario className='abazinha'></AbaUsuario>
+    function sairClick() {
 
-
-        
-        
-        
+        storage.remove('usuario-logado');
+        navigate('/')
+    }
 
 
 
-    </div>
-    
+
+
+
+
+
+    var aba = document.querySelectorAll('.botao-aba')
+
+    function selectAba() {
+        aba.forEach((item) =>
+            item.classList.remove('ativo')
+        );
+        this.classList.add('ativo')
+    }
+
+    aba.forEach((item) =>
+        item.addEventListener('click', selectAba)
+    )
+
+
+
+
+
+
+
+    return (
+
+        <div className='aba-usuario'>
+
+            <div className='superior' />
+            <section className='f1-usuario'>
+
+
+                <div className='options'>
+
+                    <div className='cima'>
+                        <img src='../../../assets/images/logo.svg' />
+                        <div>
+                            <h1> {usuario}</h1>
+                            <div className='line' />
+                            <p>{email}</p>
+                        </div>
+                    </div>
+
+
+                    <div className='baixo'>
+
+
+                        <div className='buttons'>
+                            <article className='botao-aba '>
+
+                                <button className='item-botao'>
+                                    <p>HOME</p>
+                                    <img src='../../assets/images/image142.svg'></img>
+                                </button>
+
+
+                            </article>
+
+                            <article className='botao-aba'>
+
+                                <button className='item-botao ativo'>
+                                    <p>EDITAR PERFIL</p>
+                                    <img src='../../assets/images/image138.svg'></img>
+                                </button>
+
+                            </article>
+
+                            <article className='botao-aba'>
+
+                                <button className='item-botao'>
+                                    <p>NOTIFICAÇÕES</p>
+                                    <img src='../../assets/images/image137.svg'></img>
+                                </button>
+
+                            </article>
+
+                            <article className='botao-aba'>
+
+                                <button className='item-botao'>
+                                    <p>SENHAS/SEGURANÇA</p>
+                                    <img src='../../assets/images/image139.svg'></img>
+                                </button>
+
+                            </article>
+
+
+                            <article className='botao-aba' onClick={sairClick}>
+
+                                <button className='item-botao'>
+                                    <p>SAIR DA CONTA</p>
+                                    <img src='../../assets/images/sair.png'></img>
+                                </button>
+
+                            </article>
+
+                        </div>
+
+
+                        <script src='lateral.js' />
+
+
+                    </div>
+
+                </div>
+
+                <div className='conteudo-option'>
+
+                    < Modais />
+
+                </div>
+
+
+            </section>
+
+        </div>
+
     )
 
 }
