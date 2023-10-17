ba4eb8d7007d67400e6931cb93322d5764170e01
Alezin <araujobr1903@gmail.com>
2023-10-17 18:21:26 -0300

    ...

--- site/src/pages/USUARIO/perfilusuario/index.js
+++ site/src/pages/USUARIO/perfilusuario/index.js
@@ -52,23 +52,36 @@ export default function AbaUsuario() {
     )
 
 
+    
+
+
+
+    
+
+
+
 
     function abrirAlt(){
 
         const modal = document.getElementById('editarPerfil')
-        
-        modal.classList.remove('abrir')
         modal.classList.add('abrir')
+
+        modal.classList.remove('open')
+        
+    }
+
+
+    function abrirSeg(){
+
+        const modal = document.getElementById('senhaSeguranca')
+         if(modal.classList.add('abrir')){
+            modal.classList.remove('abrir')
+         }
         
         
     }
     
-    function abrirSeguranca(){
-
-        const modal = document.getElementById('senhaSeguranca')
-        modal.classList.remove('abrir')
-        modal.classList.add('abrir')
-    }
+ 
 
    
 
@@ -100,49 +113,49 @@ export default function AbaUsuario() {
 
                     <ul className='baixo'>
                         <li className='item-menu'>
-                            <a href='#'>
+                            <div>
                                
                                 <span className='link'>Home</span>
                                 <img src='../../assets/images/image142.svg'></img>
-                            </a>
+                                                        </div>
 
                         </li>
 
-                        <li className='item-menu ativo' onClick={abrirAlt}>
-                            <a href='#'>
+                        <li className='item-menu ativo'  onClick={abrirAlt}>
+                            <div>
                               
                                 <span className='link'>Editar Perfil</span>
                                 <img src='../../assets/images/image138.svg'></img>
-                            </a>
+                                                        </div>
 
                         </li>
 
-                        <li className='item-menu'>
-                            <a href='#'>
+                        <li className='item-menu' >
+                            <div>
                                 
                                 <span className='link'>Notificação</span>
                                 <img src='../../assets/images/image137.svg'></img>
-                                
-                            </a>
+                                                            </div>
+                            
 
                         </li>
 
-                        <li className='item-menu' onClick={abrirSeguranca}>
-                            <a href='#'>
+                        <li className='item-menu'  onClick={abrirSeg}>
+                            <div>
                                 
                                 <span className='link'>Senha/Segurança</span>
                                 <img src='../../assets/images/image139.svg'></img>
-                            </a>
+                                                        </div>
 
                         </li>
 
-                        <li className='item-menu'>
-                            <a href='#'>
+                        <li className='item-menu' >
+                            <div>
                                 
                                 <span className='link'>Sair da conta</span>
                                 <img src='../../assets/images/sair.png'></img>
-
-                            </a>
+                            </div>
+                            
 
                         </li>
 
