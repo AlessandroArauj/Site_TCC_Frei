120e7fbe03cd536588c234f9994e25290bfc39c4
Thiago <ThiASouza7362@gmail.com>
2023-10-24 18:28:13 -0300

    carrinho ta faltando uns ajustes pra ta peonto

--- site/src/pages/USUARIO/carrinho/index.js
+++ site/src/pages/USUARIO/carrinho/index.js
@@ -2,6 +2,7 @@ import './index.scss'
 import Header from '../../../components/cabecalho'
 import Compcarrinho from '../../../components/compcarrinho'
 
+
 export default function Cadastrar() {
 
     return (
@@ -9,8 +10,34 @@ export default function Cadastrar() {
             <Header />
             <section>
                 <Compcarrinho />
+           
+            
               
             </section>
+            
+            <div className='baixo'>
+                <div className='esquerda'>
+            <div>
+            <p>SELECIONAR TODOS OS PRODUTOS</p>
+            <p></p>
+            <input className='box' type='checkbox'></input>
+            </div>
+
+            <div>
+            <p>TOTAL DE PRODUTOS</p>
+            <p></p>
+            <h1>20,00 RS</h1>
+            </div>
+
+           
+            </div>
+            <div className='direita'>
+            <div>
+            <button className='tirar'>Remover do carrinho</button>
+            </div>
+            </div>
+
+            </div>
         </div>
     )
 }
